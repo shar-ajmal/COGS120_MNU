d71e71d4ddf9c099fbc75ce64aa238734a829de3
Faraz <fnshaikh@ucsd.edu>
2018-11-08 00:57:57 -0800

    Update public/scripts/review.js

---
+++ public/scripts/review.js
@@ -0,0 +1,29 @@
+function render(data){
+	var html = "<div class = 'commentBox'> <div class= 'leftPanelImg'><img src='https://placeholit.imgix.net/~text?w=100&h=100' /></div><div class='rightPanel'><span>"+data.name+"</span><div class='date'>"+data.date+"</div><p>"+data.body+"</p></div><div class='clear'></div></div>";
+	$('container').append(html);
+}
+
+$(document).ready(function(){
+	var comment = [
+		{"name": "Faraz Shaikh", "date": "10 Apr, 2016", "body": "this is so cool 1"}
+	];
+
+
+		for(var i = 0; comment.length; i++){
+			render(comment[i]); 
+		}
+
+	$('#addCommnet').click(function(){
+		var addObj = {
+			"name": $('#name').val(), 
+			"date": $('#date').val(), 
+			"body": $('#bodyText').val()
+
+		};
+		commnet.push(addObj); 
+		render(addObj); 
+		$('name').val('');
+		$('#date').val('dd/mm/yyyy');
+		$('#bodyText').val('');
+	});
+});
