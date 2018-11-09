d19428b96f03ee60efbb4778d0307a877a54d038
Shaheryar Ajmal <sajmal@ucsd.edu>
2018-11-08 23:26:33 -0800

    Added Search Filter

--- public/scripts/HomePage.js
+++ public/scripts/HomePage.js
@@ -7,14 +7,44 @@ var restaurantArray = [
     'Dominos',
     'Papa Johns',
     'Pizza Hut',
-    'Assfuck',
+    'Mamas Kitchen',
 ];
 
+var itemArray = [
+    'Spicy Garlic Eggplant',
+    'Garlic Eggplant',
+    'Great Food',
+    'California Burrito',
+    'Deep Dish Pizza',
+    'OG Pasta',
+    'Carne Asada Fries',
+    'Bread Sticks',
+    'Biryani',
+];
+
+var mainArray = restaurantArray;
+var searchFilterValue = 'restaurants'
 var searchBar = document.getElementById('search-bar');
 var dropDownList = document.getElementById('restaurant-dropdown-list');
 var placeHolderDropdown = document.getElementById('placeholder-dropdown');
+var searchFilter = document.getElementById('restuarant-item-filter');
 
 searchBar.addEventListener('keyup', displayDropDown);
+searchFilter.addEventListener('click', toggleFilter);
+
+function toggleFilter(e) {
+    if (searchFilter.innerText === 'Search Menu Items') {
+        searchFilter.innerText = 'Search Restaurants';
+        mainArray = itemArray;
+        searchFilterValue = 'items'
+    }
+    else {
+        searchFilter.innerText = 'Search Menu Items'
+        mainArray = restaurantArray;
+        searchFilterValue = 'restaurants'
+    }
+    displayDropDown();
+}
 
 function getSubstringItems(array, substring) {
     substringItemList = []
@@ -30,10 +60,24 @@ function getSubstringItems(array, substring) {
 }
 
 function createDropDownItem(name) {
+    var redirectLink = ''
+    if (name.toLowerCase() === 'spicy sity'){
+        redirectLink = 'restaurant.html'
+    }
+    else if (name.toLowerCase() === 'spicy garlic eggplant') {
+        redirectLink = 'eggplant.html';
+    }
+    else if (searchFilterValue === 'restaurants'){
+        redirectLink = 'restaurant.html'
+    }  
+    else {
+        redirectLink = 'menu_dish.html'
+    }
+
     var dropdownItemContainer = document.createElement('div');
     dropdownItemContainer.classList.add('dropdown-item-container');
     dropdownItemContainer.addEventListener('click', function(e) {
-        window.location.href = 'restaurant.html';
+        window.location.href = redirectLink;
     })
     var dropdownInfoContainer = document.createElement('div')
     dropdownInfoContainer.classList.add('dropdown-info-container');
@@ -44,6 +88,7 @@ function createDropDownItem(name) {
     var dropdownName = document.createElement('div')
     dropdownName.classList.add('dropdown-name');
     dropdownName.innerText = name
+    
     dropdownInfoContainer.appendChild(dropdownPic);
     dropdownInfoContainer.appendChild(dropdownName);
     dropdownItemContainer.appendChild(dropdownInfoContainer);
@@ -59,7 +104,7 @@ function displayDropDown(e) {
         dropDownList.innerHTML = placeHolderDropdown.innerHTML;
         return;
     }
-    var prefixItems = getSubstringItems(restaurantArray, inputText)
+    var prefixItems = getSubstringItems(mainArray, inputText)
     if (prefixItems.length == 0) {
         dropDownList.innerHTML = ('<h1>No Restaurants Found</h1>');
     }
